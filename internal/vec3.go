8a24bbddbd2d424abb24ed393dd41a8b7a9f7b98
Jamie Corrigan <38882200+jamrig@users.noreply.github.com>
2025-11-04 01:18:09 +1000

    add Vec3

---
+++ internal/vec3.go
@@ -0,0 +1,73 @@
+package lumen
+
+import "math"
+
+type Vec3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+func NewVec3(x, y, z float64) Vec3 {
+	return Vec3{
+		X: x,
+		Y: y,
+		Z: z,
+	}
+}
+
+func (v *Vec3) Add(u Vec3) Vec3 {
+	return Vec3{
+		X: v.X + u.X,
+		Y: v.Y + u.Y,
+		Z: v.Z + u.Z,
+	}
+}
+
+func (v *Vec3) Sub(u Vec3) Vec3 {
+	return Vec3{
+		X: v.X - u.X,
+		Y: v.Y - u.Y,
+		Z: v.Z - u.Z,
+	}
+}
+
+func (v *Vec3) Mul(t float64) Vec3 {
+	return Vec3{
+		X: v.X * t,
+		Y: v.Y * t,
+		Z: v.Z * t,
+	}
+}
+
+func (v *Vec3) Div(t float64) Vec3 {
+	return Vec3{
+		X: v.X / t,
+		Y: v.Y / t,
+		Z: v.Z / t,
+	}
+}
+
+func (v *Vec3) Dot(u Vec3) float64 {
+	return v.X*u.X + v.Y*u.Y + v.Z*u.Z
+}
+
+func (v *Vec3) Cross(u Vec3) Vec3 {
+	return NewVec3(
+		v.Z*u.Y-v.Y*u.Z,
+		v.X*u.Z-v.X*u.Z,
+		v.Y*u.X-v.X*u.Y,
+	)
+}
+
+func (v *Vec3) LengthSquared() float64 {
+	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
+}
+
+func (v *Vec3) Length() float64 {
+	return math.Sqrt(v.LengthSquared())
+}
+
+func (v *Vec3) Unit() Vec3 {
+	return v.Div(v.Length())
+}
