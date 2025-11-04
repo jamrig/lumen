365b527588c93a9a535e5617219f3c2ff2840b06
Jamie Corrigan <38882200+jamrig@users.noreply.github.com>
2025-11-04 23:50:42 +1000

    add Fuzz to metal material

--- cmd/main.go
+++ cmd/main.go
@@ -19,8 +19,8 @@ func main() {
 	// materials
 	materialGround := material.NewLambertianMaterial(maths.NewColor(0.8, 0.8, 0.0))
 	materialCenter := material.NewLambertianMaterial(maths.NewColor(0.1, 0.2, 0.5))
-	materialLeft := material.NewMetalMaterial(maths.NewColor(0.8, 0.8, 0.8))
-	materialRight := material.NewMetalMaterial(maths.NewColor(0.8, 0.6, 0.2))
+	materialLeft := material.NewMetalMaterial(maths.NewColor(0.8, 0.8, 0.8), 0.3)
+	materialRight := material.NewMetalMaterial(maths.NewColor(0.8, 0.6, 0.2), 1.0)
 
 	// scene
 	scene := lumen.NewScene()
--- internal/lumen/material/metal.go
+++ internal/lumen/material/metal.go
@@ -4,17 +4,28 @@ import "github.com/jamrig/lumen/internal/lumen/maths"
 
 type MetalMaterial struct {
 	Albedo maths.Color
+	Fuzz   float64
 }
 
-func NewMetalMaterial(albedo maths.Color) MetalMaterial {
+func NewMetalMaterial(albedo maths.Color, fuzz float64) MetalMaterial {
 	return MetalMaterial{
 		Albedo: albedo,
+		Fuzz:   fuzz,
 	}
 }
 
 func (m MetalMaterial) Scatter(hit *maths.Intersection) *maths.ScatteredRay {
-	dir := hit.Ray.Direction.Reflect(hit.Normal)
-	r := maths.NewScatteredRay(maths.NewRay(hit.Point, dir), m.Albedo)
+	reflected := hit.Ray.Direction.Reflect(hit.Normal)
+
+	if m.Fuzz > 0 {
+		reflected = reflected.Unit().Add(maths.NewRandomUnitVec3().Mul(m.Fuzz))
+	}
+
+	if reflected.Dot(hit.Normal) <= 0 {
+		return nil
+	}
+
+	r := maths.NewScatteredRay(maths.NewRay(hit.Point, reflected), m.Albedo)
 
 	return &r
 }
